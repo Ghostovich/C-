c723c6eaef7120186b30f6301c36fd755b8af6e0
Konstantin <gusarov_konstant@mail.ru>
2022-09-03 12:56:56 +0300

    007

---
+++ example007/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1; 
+int b = 3; 
+int c = 4;
+int d = 8;
+int e = 6;
+
+int max = a;
+if (b>max) b = max;
+if (c>max) c = max;
+if (d>max) d = max;
+if (e>max) e = max;
+
+Console.Write("Максимальное число:");
+Console.WriteLine(max);
